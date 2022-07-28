98e3931108d391f4ae49bd369201b8ff5d9ce24b
onlyhappiness <onlyhappiness96@gmail.com>
2022-07-28 14:55:04 +0900

    [ Setup ] Swagger

---
+++ swagger/index.js
@@ -0,0 +1,27 @@
+const swaggerUi = require("swagger-ui-express");
+const swaggereJsdoc = require("swagger-jsdoc");
+
+const serverUrl = `https:${process.env.SERVER_IP}:${process.env.PORT}`;
+
+const options = {
+  swaggerDefinition: {
+    info: {
+      title: "PLZ",
+      version: "1.0.0",
+      description: "PLZ SWAGGER",
+    },
+    servers: [
+      {
+        url: "https://plz-server.herokuapp.com/",
+      },
+    ],
+  },
+  apis: ["./controllers/User/*.js"],
+};
+
+const specs = swaggereJsdoc(options);
+
+module.exports = {
+  swaggerUi,
+  specs,
+};
