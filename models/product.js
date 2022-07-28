cea9f760887e035067d7157472c274a85ae2f690
onlyhappiness <onlyhappiness96@gmail.com>
2022-07-28 15:23:45 +0900

    [ SetUp ] Product Modeling

---
+++ models/product.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+  },
+  // thumbnail: {
+  // }
+  description: {
+    type: String,
+  },
+  cost: {
+    type: Number,
+  },
+  status: {
+    type: Number,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = { Product };
