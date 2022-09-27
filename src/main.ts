31625cb0fb04606fdbd431651b81e89299465f6b
onlyhappiness <onlyhappiness96@gmail.com>
2022-09-27 21:36:26 +0900

    [ init ] Logger Middleware

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // const PORT = process.env.PORT;
   const PORT = 7700;
 
-  await app.listen(PORT);
+  await app.listen(PORT, () => {
+    console.log(`running on ${PORT}...`);
+  });
 }
 bootstrap();
