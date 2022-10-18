48fca88a00e8c2eaae95eebe84b40a31b0c6bf86
onlyhappiness <onlyhappiness96@gmail.com>
2022-10-18 22:05:58 +0900

    :sparkles: Feat: 장바구니 controller 및 service 세팅

---
+++ src/cart/cart.schema.ts
@@ -0,0 +1,48 @@
+import { Prop, Schema, SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber } from 'class-validator';
+import { Document, Types } from 'mongoose';
+
+const options: SchemaOptions = {
+  timestamps: true,
+};
+
+@Schema(options)
+export class Cart extends Document {
+  //** 유저 */
+  @ApiProperty({
+    description: '유저 id',
+    required: true,
+  })
+  @Prop({
+    type: Types.ObjectId,
+    required: true,
+    ref: 'users',
+  })
+  @IsNotEmpty()
+  user: Types.ObjectId;
+
+  //** 상품 */
+  @ApiProperty({
+    description: '상품 id',
+    required: true,
+  })
+  @Prop({
+    type: Types.ObjectId,
+    required: true,
+    ref: 'products',
+  })
+  @IsNotEmpty()
+  product: Types.ObjectId;
+
+  //** 수량 */
+  @ApiProperty({
+    description: '수량',
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  count: number;
+}
+
+export const CartSchema = SchemaFactory.createForClass(Cart);
