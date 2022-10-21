3bc667f38e3a30ac3e3f111169371609ae0e174f
onlyhappiness <onlyhappiness96@gmail.com>
2022-10-21 17:23:43 +0900

    :recycle: Feat: Port 변경

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(process.env.PORT || 3000, () => {
+  await app.listen(process.env.PORT, () => {
     console.log('success!');
   });
 }
